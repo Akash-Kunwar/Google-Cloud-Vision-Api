8a9a34974cdc77e1112b809e826f5d49c1804163
Akash-Kunwar <48173287+Akash-Kunwar@users.noreply.github.com>
2020-03-04 22:02:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,230 @@
+package com.example.test;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.provider.MediaStore;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.ColorInfo;
+import com.google.api.services.vision.v1.model.DominantColorsAnnotation;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.ImageProperties;
+import com.google.api.services.vision.v1.model.SafeSearchAnnotation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+    private Feature feature;
+    ImageView imageView;
+    TextView visionAPIData;
+    ProgressBar imageUploadProgress;
+    Button takepicture;
+    private static final String TAG = "MainActivity";
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyBSX6fMXZO2dEnsZmwMaK05_S-SrOQO-Ho";
+    private String[] visionAPI = new String[]{"LANDMARK_DETECTION", "LOGO_DETECTION", "SAFE_SEARCH_DETECTION", "IMAGE_PROPERTIES", "LABEL_DETECTION"};
+
+    private String api = visionAPI[0];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=(ImageView) findViewById(R.id.imageview);
+        imageUploadProgress=(ProgressBar)findViewById(R.id.progressbar);
+        visionAPIData=(TextView)findViewById(R.id.visionAPIData);
+        takepicture=(Button)findViewById(R.id.takepicture);
+        feature = new Feature();
+        feature.setType(visionAPI[0]);
+        feature.setMaxResults(10);
+        imageUploadProgress.setVisibility(View.INVISIBLE);
+        takepicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePictureFromCamera();
+            }
+        });
+    }
+    public void takePictureFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+            callCloudVision(bitmap, feature);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length == 0 && grantResults[0] == PackageManager.PERMISSION_DENIED
+                    && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                finish();
+            } else {
+                takepicture.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+    private void callCloudVision(final Bitmap bitmap, final Feature feature) {
+        imageUploadProgress.setVisibility(View.VISIBLE);
+        final List<Feature> featureList = new ArrayList<>();
+        featureList.add(feature);
+
+        final List<AnnotateImageRequest> annotateImageRequests = new ArrayList<>();
+
+        AnnotateImageRequest annotateImageReq = new AnnotateImageRequest();
+        annotateImageReq.setFeatures(featureList);
+        annotateImageReq.setImage(getImageEncodeImage(bitmap));
+        annotateImageRequests.add(annotateImageReq);
+
+
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    VisionRequestInitializer requestInitializer = new VisionRequestInitializer(CLOUD_VISION_API_KEY);
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(requestInitializer);
+
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(annotateImageRequests);
+
+                    Vision.Images.Annotate annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    return convertResponseToString(response);
+                } catch (GoogleJsonResponseException e) {
+                    Log.d(TAG, "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(TAG, "failed to make API request because of other IOException " + e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                visionAPIData.setText(result);
+                imageUploadProgress.setVisibility(View.INVISIBLE);
+            }
+        }.execute();
+    }
+    @NonNull
+    private Image getImageEncodeImage(Bitmap bitmap) {
+        Image base64EncodedImage = new Image();
+        // Convert the bitmap to a JPEG
+        // Just in case it's a format that Android understands but Cloud Vision
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        // Base64 encode the JPEG
+        base64EncodedImage.encodeContent(imageBytes);
+        return base64EncodedImage;
+    }
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+
+        AnnotateImageResponse imageResponses = response.getResponses().get(0);
+
+        List<EntityAnnotation> entityAnnotations;
+
+        String message = "";
+        switch (api) {
+            case "LANDMARK_DETECTION":
+                entityAnnotations = imageResponses.getLandmarkAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "LOGO_DETECTION":
+                entityAnnotations = imageResponses.getLogoAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+            case "SAFE_SEARCH_DETECTION":
+                SafeSearchAnnotation annotation = imageResponses.getSafeSearchAnnotation();
+                message = getImageAnnotation(annotation);
+                break;
+            case "IMAGE_PROPERTIES":
+                ImageProperties imageProperties = imageResponses.getImagePropertiesAnnotation();
+                message = getImageProperty(imageProperties);
+                break;
+            case "LABEL_DETECTION":
+                entityAnnotations = imageResponses.getLabelAnnotations();
+                message = formatAnnotation(entityAnnotations);
+                break;
+        }
+        return message;
+    }
+    private String getImageProperty(ImageProperties imageProperties) {
+        String message = "";
+        DominantColorsAnnotation colors = imageProperties.getDominantColors();
+        for (ColorInfo color : colors.getColors()) {
+            message = message + "" + color.getPixelFraction() + " - " + color.getColor().getRed() + " - " + color.getColor().getGreen() + " - " + color.getColor().getBlue();
+            message = message + "\n";
+        }
+        return message;
+    }
+
+    private String formatAnnotation(List<EntityAnnotation> entityAnnotation) {
+        String message = "";
+
+        if (entityAnnotation != null) {
+            for (EntityAnnotation entity : entityAnnotation) {
+                message = message + "    " + entity.getDescription() + " " + entity.getScore();
+                message += "\n";
+            }
+        } else {
+            message = "Nothing Found";
+        }
+        return message;
+    }
+    private String getImageAnnotation(SafeSearchAnnotation annotation) {
+        return String.format("adult: %s\nmedical: %s\nspoofed: %s\nviolence: %s\n",
+                annotation.getAdult(),
+                annotation.getMedical(),
+                annotation.getSpoof(),
+                annotation.getViolence());
+    }
+
+}
